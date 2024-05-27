a37cf7848a01aaa3aa4529073ac8683423f89311
Buddhika Weerasinghe <54759523+TurboJapuraEfac@users.noreply.github.com>
2024-05-27 16:05:58 +0530

    Groq API based LLM application

---
+++ groq/app.py
@@ -0,0 +1,58 @@
+
+import os
+import time
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq # To use groq API
+from langchain.chains import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate # Custom chat prompts
+from langchain_community.vectorstores.faiss import FAISS # Vector DB
+from langchain_community.embeddings import OllamaEmbeddings # Ollama Embeddings for free
+from langchain_community.document_loaders import WebBaseLoader # Read a website
+from langchain_text_splitters import RecursiveCharacterTextSplitter # Convert documents to smaller chunks
+from langchain.chains.combine_documents import create_stuff_documents_chain 
+
+load_dotenv()
+
+# Use Llama 3 based embeddings
+ollama_emb = OllamaEmbeddings(model="llama3")
+groq_api_key = os.environ['GROQ_API_KEY']
+print(groq_api_key)
+
+if "vector" not in st.session_state:
+    st.session_state.embeddings = ollama_emb
+    st.session_state.loader = WebBaseLoader("https://en.wikipedia.org/wiki/Sri_Lanka")
+    st.session_state.docs = st.session_state.loader.load()
+    st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+    st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+
+st.title("ChatGroq Demo")
+llm = ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-70b-8192")
+
+prompt = ChatPromptTemplate.from_template( 
+        """
+        Answer the questions based on the provided context only.
+        Please provide the most accurate response based on the question
+        <context>
+        {context}
+        <context>
+        Questions:{input} """)
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever = st.session_state.vectors.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+prompt = st.text_input("Input you prompt here")
+
+if prompt:
+    start = time.process_time()
+    response = retrieval_chain.invoke({"input":prompt})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
