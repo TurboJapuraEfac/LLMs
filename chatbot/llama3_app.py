55610d0bbd00b522c999311caaeec867697b574a
Buddhika Weerasinghe <54759523+TurboJapuraEfac@users.noreply.github.com>
2024-05-27 16:04:48 +0530

    OpenAI and LLama3 based chatbots

---
+++ chatbot/llama3_app.py
@@ -0,0 +1,31 @@
+
+import os
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+## streamlit framework
+st.title('Langchain Demo With LLAMA3 API')
+input_text=st.text_input("Search the topic u want")
+
+# ollama LLAma3 LLm 
+llm=Ollama(model="llama3")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
